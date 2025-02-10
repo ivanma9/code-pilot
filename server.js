7591724612847f98fdd0f97b87a5f61ed0c5dc4b
Ivan Ma <ivanma819@gmail.com>
2025-02-10 10:40:10 -0800

    Remove server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-app.get('/config.js', (req, res) => {
-  res.set('Content-Type', 'application/javascript')
-  res.send(`
-    export const config = {
-      GROQ_API_KEY: '${process.env.GROQ_API_KEY}'
-    };
-  `)
-})
